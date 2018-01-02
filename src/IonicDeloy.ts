950b3517be6364b9a0dc62e8a560e750b723792f
huangguochao <917929050@qq.com>
2018-01-02 23:22:54 +0800

    强势！！添加热更新功能。

--- src/IonicDeloy.ts
+++ src/IonicDeloy.ts
@@ -1,4 +1,4 @@
-import {Injectable} from "@angular/core";
+import {Injectable, NgZone} from "@angular/core";
 import {Platform} from "ionic-angular";
 
 declare var IonicCordova;
@@ -10,7 +10,8 @@ export class IonicDeploy{
   private pluginWasInitialized: boolean = false;
   private _channel: string = 'Master';
 
-  constructor(private platform: Platform) {
+  constructor(private platform: Platform,
+              private zone: NgZone) {
     this.isCordovaEnv = platform.is('cordova');
   }
 
@@ -25,10 +26,86 @@ export class IonicDeploy{
     });
   }
 
+  // 检查是否有可用更新
+  check() {
+    this.onlyIfPluginInitialized();
+    return new Promise((resolve, reject) => {
+      IonicCordova.deploy.check(result => resolve((result === 'true') ? true : false), err => reject(err));
+    });
+  }
+
+  // 一个新版本可用下载
+  download(onProgress) {
+    this.onlyIfPluginInitialized();
+    return new Promise((resolve, reject) => {
+      IonicCordova.deploy.download(result => {
+        const percent = (result === 'true' || result === 100) ? 100 : result;
+        this.zone.run(() => onProgress(percent));
+        if (percent === 100) resolve();
+      }, err => {
+        reject(err);
+      });
+    });
+  }
+
+  // 解压最新版本
+  extract(onProgress) {
+    this.onlyIfPluginInitialized();
+    return new Promise((resolve, reject) => {
+      IonicCordova.deploy.extract(result => {
+        const percent = (result === 'done') ? 100 : result;
+        this.zone.run(() => onProgress(percent));
+        if (percent === 100) resolve();
+      }, err => {
+        reject(err);
+      });
+    });
+  }
+
+  // 加载最新版本
+  load() {
+    this.onlyIfPluginInitialized();
+    return new Promise((resolve, reject) => {
+      IonicCordova.deploy.redirect(() => resolve(true), err => reject(err));
+    });
+  }
+
+  // 获取该设备的当前版本信息
+  info() {
+    this.onlyIfPluginInitialized();
+    return new Promise((resolve, reject) => {
+      IonicCordova.deploy.info(result => resolve(result), err => reject(err));
+    });
+  }
+
+  // 获取该设备中已经下载的版本
+  getVersions() {
+    this.onlyIfPluginInitialized();
+    return new Promise((resolve, reject) => {
+      IonicCordova.deploy.getVersions(result => resolve(result), err => reject(err));
+    });
+  }
+
+  // 根据UUID删除该设备已经下载的版本
+  deleteVersion(uuid) {
+    this.onlyIfPluginInitialized();
+    return new Promise((resolve, reject) => {
+      IonicCordova.deploy.deleteVersion(uuid, () => resolve(true), err => reject(err));
+    });
+  }
+
   // 判断是否为Cordova环境
   private onlyIfCordovaEnv() {
     if (!this.isCordovaEnv) {
-      console.error('IonicDeploy was not runned in cordova environment!');
+      console.error('IonicDeploy不能在非真实机器上运行！');
+      return;
+    }
+  }
+
+  // 检查插件是否已经初始化
+  private onlyIfPluginInitialized() {
+    if (!this.pluginWasInitialized) {
+      console.error('IonicDeploy未被初始化, 你需要先执行init方法！');
       return;
     }
   }
--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController, Platform, ToastController} from 'ionic-angular';
 import {IonicDeploy} from "../../IonicDeloy";
 
 @Component({
@@ -8,13 +8,47 @@ import {IonicDeploy} from "../../IonicDeloy";
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController,
-              public ionicDeplay: IonicDeploy) {
+  // 当前是否在运行版本更新
+  runningDeploy: boolean = false;
+
+  constructor(private ionicDeploy: IonicDeploy,
+              private platform: Platform,
+              public toastCtrl: ToastController) {
 
   }
 
   update(){
-    this.ionicDeplay.init();
+
+    if (this.platform.is('cordova')) {
+      if (this.runningDeploy) return;
+
+      let toast = this.toastCtrl.create({
+        message: '下载中 ... 0%',
+        position: 'top',
+        showCloseButton: false,
+        closeButtonText: '关闭'
+      });
+
+      this.ionicDeploy.init()
+        .then(() => this.ionicDeploy.check())
+        .then(snapshotAvailable => {
+          if (snapshotAvailable) {
+            toast.present();
+            this.runningDeploy = true;
+            return this.ionicDeploy.download(percent => toast.setMessage('下载中 ... ' + percent + '%'));
+          }
+        })
+        .then(() => this.ionicDeploy.extract(percent => toast.setMessage('解压中 ... ' + percent + '%')))
+        .then(() => this.ionicDeploy.load())
+        .then(() => toast.dismiss())
+        .then(() => this.runningDeploy = false)
+        .catch(() => {
+          this.runningDeploy = false;
+          toast.setMessage('对不起，由于网络问题更新失败, 请在次手动更新！');
+        })
+    }else{
+      console.error('版本热更新不能在非真实机器上运行！')
+    }
   }
 
 }
